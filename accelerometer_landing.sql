4a5b479f3048a80f710fa074dd205d1e001bc036
xantsvx1 <asoto50@wgu.edu>
2026-04-26 18:47:53 -0400

    Create accelerometer_landing.sql
    Add accelerometer landing SQL script

---
+++ accelerometer_landing.sql
@@ -0,0 +1,12 @@
+CREATE EXTERNAL TABLE `accelerometer_landing`(
+)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://accelerometer-s3-landing/accelerometer/landing/'
+TBLPROPERTIES (
+  'classification'='json')
