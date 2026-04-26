21e4079ac2154ba51e395e91778e317e0f400872
xantsvx1 <asoto50@wgu.edu>
2026-04-26 18:46:08 -0400

    Create customer_landing.sql
    Add customer landing SQL script

---
+++ customer_landing.sql
@@ -0,0 +1,12 @@
+CREATE EXTERNAL TABLE `customer_landing`(
+)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://customer-s3-landing/customer/landing/'
+TBLPROPERTIES (
+  'classification'='json')
