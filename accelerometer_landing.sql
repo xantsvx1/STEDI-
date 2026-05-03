43ba3722cbdd4295913ceb46cd194391085c496a
xantsvx1 <asoto50@wgu.edu>
2026-05-03 19:57:46 -0400

    accelerometer_landing.sql
    SQL to create accelerometer_landing table

--- accelerometer_landing.sql
+++ accelerometer_landing.sql
@@ -1,12 +1,28 @@
 CREATE EXTERNAL TABLE `accelerometer_landing`(
-)
+  `user` string COMMENT 'from deserializer', 
+  `timestamp` bigint COMMENT 'from deserializer', 
+  `x` double COMMENT 'from deserializer', 
+  `y` double COMMENT 'from deserializer', 
+  `z` double COMMENT 'from deserializer')
 ROW FORMAT SERDE 
   'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'paths'='timestamp,user,x,y,z') 
 STORED AS INPUTFORMAT 
   'org.apache.hadoop.mapred.TextInputFormat' 
 OUTPUTFORMAT 
   'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
 LOCATION
-  's3://accelerometer-s3-landing/'
+  's3://accelerometer-s3-landing/accelerometer_landing/'
 TBLPROPERTIES (
-  'classification'='json')
+  'CRAWL_RUN_ID'='e0e17ec4-c515-473f-b5a0-eb31ab83c6ad', 
+  'CrawlerSchemaDeserializerVersion'='1.0', 
+  'CrawlerSchemaSerializerVersion'='1.0', 
+  'UPDATED_BY_CRAWLER'='accelerometer_landing_crawler', 
+  'averageRecordSize'='761', 
+  'classification'='json', 
+  'compressionType'='none', 
+  'objectCount'='9', 
+  'recordCount'='9007', 
+  'sizeKey'='6871328', 
+  'typeOfData'='file')
