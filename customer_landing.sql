1caa2f8ededbbb2dfd5c1b87cf3ede63fee036bf
xantsvx1 <asoto50@wgu.edu>
2026-05-03 19:56:47 -0400

    customer_landing.sql
    SQL to create customer_landing table

--- customer_landing.sql
+++ customer_landing.sql
@@ -1,12 +1,33 @@
 CREATE EXTERNAL TABLE `customer_landing`(
-)
+  `customername` string COMMENT 'from deserializer', 
+  `email` string COMMENT 'from deserializer', 
+  `phone` string COMMENT 'from deserializer', 
+  `birthday` string COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `registrationdate` bigint COMMENT 'from deserializer', 
+  `lastupdatedate` bigint COMMENT 'from deserializer', 
+  `sharewithresearchasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithpublicasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithfriendsasofdate` bigint COMMENT 'from deserializer')
 ROW FORMAT SERDE 
   'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'paths'='birthDay,customerName,email,lastUpdateDate,phone,registrationDate,serialNumber,shareWithFriendsAsOfDate,shareWithPublicAsOfDate,shareWithResearchAsOfDate') 
 STORED AS INPUTFORMAT 
   'org.apache.hadoop.mapred.TextInputFormat' 
 OUTPUTFORMAT 
   'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
 LOCATION
-  's3://customer-s3-landing/'
+  's3://customer-s3-landing/customer_landing/'
 TBLPROPERTIES (
-  'classification'='json')
+  'CRAWL_RUN_ID'='73f423ed-e845-4f0c-aa5c-9f7ec199d1c0', 
+  'CrawlerSchemaDeserializerVersion'='1.0', 
+  'CrawlerSchemaSerializerVersion'='1.0', 
+  'UPDATED_BY_CRAWLER'='customer_landing_crawler', 
+  'averageRecordSize'='307', 
+  'classification'='json', 
+  'compressionType'='none', 
+  'objectCount'='1', 
+  'recordCount'='956', 
+  'sizeKey'='293777', 
+  'typeOfData'='file')
